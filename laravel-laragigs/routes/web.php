ad13879d78f214d4b96461b695ad0c5ad6602392
Biruk-Dereje <derejebiruk1@gmail.com>
2024-08-11 05:01:15 -0700

    view,listingfactory commit

--- laravel-laragigs/routes/web.php
+++ laravel-laragigs/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
+
+//All listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'latest Listing',
+        'listings' => Listing::all()
+    ]);
+});
+
+
+// single listing
+Route::get('/listing/{id}', function($id){
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
 });
